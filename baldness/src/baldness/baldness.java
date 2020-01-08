03fd34106fba5acd6a69ae12e655485ccb2b401c
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2020-01-08 12:16:24 -0700

    Hello, I added a print statement and a for loop.

--- baldness/src/baldness/baldness.java
+++ baldness/src/baldness/baldness.java
@@ -6,7 +6,13 @@ public class baldness
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
-System.out.println("Be bald");
+				System.out.println("Be bald");
+				
+				
+				System.out.println("NO U SPOON");
+				for(int i = 0; i < 10; i++) {
+					System.out.print("gigiddy ");
+				}
 			}
 
 	}
