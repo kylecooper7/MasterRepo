591eb5d06b47a4dc44f1eccb87b1adfe5c5efaae
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-01-09 11:16:38 -0700

    Fixing all issues (Hopefully)

--- baldness/src/baldness/baldness.java
+++ baldness/src/baldness/baldness.java
@@ -1,33 +1,41 @@
 package baldness;
 
+import java.util.Scanner;
+
 public class baldness
 	{
+		static Scanner userIntInput = new Scanner(System.in);
 
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
 
+				for (int i = 0; i < 10; i++)
+					{
+						System.out.print("gigiddy ");
+					}
 
-				
 				printNumberOneToTen();
+				
+				System.out.println();
+				System.out.println("To add numbers please input ntwo numbers below:");
+				int numberOne = userIntInput.nextInt();
+				int numberTwo = userIntInput.nextInt();
+				int sumOfNumbers = sumUpNumbers(numberOne, numberTwo);
+
+				System.out.println("The sum of the Numbers is " + sumOfNumbers);
 
 			}
 
-				System.out.println("Be bald");
-				
-				
-				System.out.println("NO U SPOON");
-				for(int i = 0; i < 10; i++) {
-					System.out.print("gigiddy ");
-					
-				}
-
-
 		private static void printNumberOneToTen()
 			{
-				// TODO Auto-generated method stub
 				for (int i = 1; i <= 10; i++)
 					System.out.print(i + " ");
+			}
+
+		private static int sumUpNumbers(int x, int y)
+			{
+				return x + y;
 			}
 
 	}
